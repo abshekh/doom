84435b0b559a9fb24a6537e2423e889d6b7393ef
Abhishek Singh <abhishek17513@gmail.com>
2022-12-18 12:42:14 +0530

    git blame changes

--- config.el
+++ config.el
@@ -209,6 +209,14 @@
 (setq auto-revert-check-vc-info t)
 (with-eval-after-load 'magit-mode
   (add-hook 'after-save-hook 'magit-after-save-refresh-status t))
+(setq magit-blame-styles ;; vertical blame
+      '((margin
+         (margin-width . 60)
+         (margin-format . ("%.6H %a %C %s"))
+         (margin-face . (magit-blame-margin))
+         (margin-body-face . (magit-blame-dimmed))
+         (show-message . t)
+         )))
 
 ;; Enable line numbers and customize their format.
 ;; (column-number-mode)
@@ -345,3 +353,40 @@
 (evil-collection-define-key 'normal 'pdf-occur-buffer-mode-map
   "n" (lambda () (interactive) (forward-line) (pdf-occur-view-occurrence))
   "N" (lambda () (interactive) (forward-line -1) (pdf-occur-view-occurrence)))
+
+;; (define-minor-mode evil-pdf-tools-mode
+;;   "A minor mode to add evil key bindings to pdf-tools."
+;;   :lighter " evil-pdf-tools"
+;;   :keymap (let ((map (make-sparse-keymap)))
+;; 	    (define-key map "k" 'pdf-view-previous-line-or-previous-page)
+;; 	    (define-key map "j" 'pdf-view-next-line-or-next-page)
+;; 	    (define-key map "l" 'image-forward-hscroll)
+;; 	    (define-key map "h" 'image-backward-hscroll)
+;; 	    (define-key map (kbd "C-f") 'pdf-view-scroll-up-or-next-page)
+;; 	    (define-key map (kbd "C-b") 'pdf-view-scroll-down-or-previous-page)
+;; 	    (define-key map "gg" 'pdf-view-first-page)
+;; 	    (define-key map "G" 'pdf-view-last-page)
+;; 	    (define-key map "r" 'revert-buffer)
+;; 	    (define-key map ":" 'evil-ex)
+;; 	    (define-key map "/" 'isearch-forward) map)
+;;   (evil-pdf-tools-setup))
+
+;; (defun evil-pdf-tools-setup ()
+;;   "Modify isearch to behave like evil search."
+;;   (progn
+;;     (add-function :before (symbol-function 'isearch-forward) #'evil-like-search-setup)
+;;     (add-function :after (symbol-function 'isearch-forward) #'evil-like-search-setup)))
+
+;; (defun evil-like-search-setup (&optional ARG PRED)
+;;   "After enter is pressed in isearch the letters n and N are used to navigate the results."
+;;   (progn
+;;     (define-key isearch-mode-map (kbd "<return>")
+;;       '(lambda () (interactive)
+;; 	 (progn
+;; 	   (define-key isearch-mode-map "n" 'isearch-repeat-forward)
+;; 	   (define-key isearch-mode-map "N" 'isearch-repeat-backward))))
+;;     (define-key isearch-mode-map "n" 'isearch-printing-char)
+;;     (define-key isearch-mode-map "N" 'isearch-printing-char)))
+
+;; ;;;###autoload
+;; (add-hook 'pdf-view-mode-hook 'evil-pdf-tools-mode)
